6cac734009dce4a9fcd90e8272134291f4e97ba8
Mads Anthony <mail@madsanthony.com>
2019-06-26 11:43:18 +0200

    Added animationHelper.

---
+++ Code/AnimationHelper.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public static class AnimationHelper {
+    public static IEnumerator AnimateLocalPosition(float duration, Transform transform, Vector3 startPos,
+        Vector3 endPos, AnimationCurve curve = null) {
+        var moveVector = endPos - startPos;
+        yield return Animate(duration, (t) => { transform.localPosition = startPos + t * moveVector; }, curve);
+    }
+
+    public static IEnumerator AnimateLocalScale(float duration, Transform transform, Vector3 startScale,
+        Vector3 endScale, AnimationCurve curve = null) {
+        var scaleVector = endScale - startScale;
+        yield return Animate(duration, (t) => { transform.localScale = startScale + t * scaleVector; }, curve);
+    }
+    
+    public static IEnumerator AnimateLocalEulerAngles(float duration, Transform transform, Vector3 startAngle,
+        Vector3 endAngle, AnimationCurve curve = null) {
+        var angleVector = endAngle - startAngle;
+        yield return Animate(duration, (t) => { transform.localEulerAngles = startAngle + t * angleVector; }, curve);
+    }
+    
+    public static IEnumerator ShakeLocalPosition(float duration, Transform transform, Vector3 startPos, AnimationCurve curve = null,
+        float amount = 0.2f, float tremor = 15, float phase = 0) {
+        
+        var p = Vector3.zero;
+        yield return Animate(duration, (t) => {
+            p.x = Mathf.Sin(t * tremor + phase) * t * amount;
+            p.y = Mathf.Cos(t * tremor * 0.6f + phase) * t * amount;
+            transform.localPosition = startPos + p;
+        }, curve);
+        yield return AnimateLocalPosition(0.05f, transform, transform.localPosition,startPos);
+    }
+
+    public static IEnumerator AnimateParallel(MonoBehaviour monoBehaviour, params IEnumerator[] enumerators) {
+        int runCount = 0;
+        foreach (var enumerator in enumerators) {
+            runCount += 1;
+            monoBehaviour.StartCoroutine(RunWithCallBack(enumerator, ()=> runCount -= 1));
+        }
+
+        while (runCount>0) {
+            yield return null;
+        }
+    }
+
+    public static IEnumerator RunWithCallBack(IEnumerator enumerator, Action whenDone) {
+        yield return enumerator;
+        whenDone();
+    }
+
+    public static IEnumerator Animate(float duration, Action<float> lerpFunction, AnimationCurve curve = null) {
+        if (curve == null) {
+            curve = AnimationCurve.Linear(0, 0, 1, 1);
+        }
+        
+        float t = 0;
+        while (t < 1) {
+            t += Time.deltaTime / duration;
+           
+            lerpFunction(curve.Evaluate(Mathf.Clamp01(t)));
+
+            yield return null;
+        }
+
+        lerpFunction(curve.Evaluate(Mathf.Clamp01(1)));
+    }
+}
